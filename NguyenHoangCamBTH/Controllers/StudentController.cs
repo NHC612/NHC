bb000ab80d37d59430391b08bc1e2505db4485ea
NHC612 <hcam418@gmail.com>
2023-02-21 22:42:08 +0700

    2202

---
+++ NguyenHoangCamBTH/Controllers/StudentController.cs
@@ -0,0 +1,14 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using NguyenHoangCamBTH.Models;
+
+
+namespace BTH2002.Controllers;
+    public class StudentController : Controller
+    {
+       
+       public IActionResult About()
+    {
+        return View();
+    }
+    }
