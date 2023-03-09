a31477004299e4df27bb6054c1071368c9934bac
NHC612 <hcam418@gmail.com>
2023-03-09 17:00:22 +0700

    0703

--- NguyenHoangCamBTH/Controllers/EmployeeController.cs
+++ NguyenHoangCamBTH/Controllers/EmployeeController.cs
@@ -2,12 +2,33 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using NguyenHoangCamBTH.Models;
 
+
 namespace NguyenHoangCamBTH.Controllers;
 
     public class EmployeeController : Controller 
-    {
+    {   
         public IActionResult Index()
-    {
-        return View();
-    }
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string FullName)
+        {       
+            string strReturn = "Hello" + FullName;
+            // gui du lieu ve view
+            ViewBag.abc    = strReturn;
+            return View();
+            // tra ve danh sach cac sinh vien trong Database
+        }
+
+        public IActionResult Creat()
+        {
+            return View();
+        }
+        
+        public IActionResult About()
+        {
+            return View();
+        }
     }
