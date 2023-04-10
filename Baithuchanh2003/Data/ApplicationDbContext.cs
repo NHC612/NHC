ce16f749c9c2d7898b34777e5b79bc3b22e4a281
NHC612 <hcam418@gmail.com>
2023-04-10 10:19:48 +0700

    2703

--- Baithuchanh2003/Data/ApplicationDbContext.cs
+++ Baithuchanh2003/Data/ApplicationDbContext.cs
@@ -8,8 +8,8 @@ namespace Baithuchanh2003.Data;
         public ApplicationDbContext (DbContextOptions<ApplicationDbContext> options) : base(options)
         {
         }
-        public DbSet<Student> Students {get; set;}
+        public DbSet<Student> Students {get; set; }
         public DbSet<Employee> Employee {get;set;  }
         public DbSet<Customer> Customer {get;set;  }
-        public DbSet<Person> Person {get;set;}
+        public DbSet<Person> Person {get;set; }
     }
--- Baithuchanh2003/Models/Student.cs
+++ Baithuchanh2003/Models/Student.cs
@@ -3,7 +3,7 @@ namespace Baithuchanh2003.Models
 {
     public class Student
     {
-       [Key]
+        [Key]
         public string StudentID { get; set; }
         public string StudentName { get; set; } //prop
         public string Address { get; set; }
